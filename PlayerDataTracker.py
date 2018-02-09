117c7ffc3f20af5b362343d2a51a352de158a17e
Justin Wei <jlw141@duke.edu>
2018-02-08 23:26:16 -0500

    graph of all average player efficiencies done

---
+++ PlayerDataTracker.py
@@ -0,0 +1,58 @@
+import csv
+
+class Player:
+    def __init__(self, n):
+        self.name = n
+        self.gameno = []
+        self.eff = []
+        self.totalEff = []
+
+    def addGame(self, gn, eff):
+        eff = int(eff)
+        self.gameno.append(gn)
+        self.eff.append(eff)
+
+        totalEff = 0
+        for i in range(len(self.gameno)): # iterate through all previous scores
+            totalEff += self.eff[i]
+        self.totalEff.append(totalEff)
+
+    def getName(self):
+        return self.name
+    def getGameNos(self):
+        return self.gameno
+    def getEffs(self):
+        return self.eff
+    def getAverageEffs(self):
+        avgEffs = []
+        counter = 0
+        for i in range(len(self.totalEff)):
+            avgEffs.append(self.totalEff[i]/(int(i)+1))
+        return avgEffs
+
+class PlayerDataTracker:
+    def __init__(self):
+        self.players = {}
+        self.readFile()
+
+    def readFile(self):
+        with open('./mbb_player_data.csv', newline='', errors='ignore') as playerFile:
+            reader = csv.DictReader(playerFile, delimiter=',')
+            # read through entries, adding to neighbourhood list and price per neighbourhood dictionary
+            for row in reader:
+                if row["player"] in self.players:
+                    self.players[row["player"]].addGame(row["gameno"], row["eff"])
+                else:
+                    self.players[row["player"]] = Player(row["player"])
+
+    def getXVals(self):
+        return self.xVals
+
+    def getYVals(self):
+        return self.yVals
+
+    def getPlayers(self):
+        listOfPlayers = []
+        for name, playerObj in self.players.items():
+             listOfPlayers.append(playerObj)
+        return listOfPlayers
--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
 import os
+from PlayerDataTracker import PlayerDataTracker
+
 
 app = dash.Dash(__name__)
 server = app.server
 
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
+pdt = PlayerDataTracker()
+playerList = pdt.getPlayers()
+
+playerGraphs = []
+for player in playerList:
+    p = go.Scatter(
+        x = player.getGameNos(),
+        y = player.getAverageEffs(),
+        mode = 'lines',
+        name = player.getName()
+    )
+    playerGraphs.append(p)
 
 app.layout = html.Div([
     html.H2('Hello, Team!'),
@@ -15,11 +31,28 @@ app.layout = html.Div([
         options=[{'label': i, 'value': i} for i in ['Justin Wei', 'Jonathan Michala', 'Miles Todzo']],
         value='Justin Wei'
     ),
-    html.Div(id='display-value')
+    html.Div(id='display-value'),
+    dcc.Graph(
+        figure=go.Figure(
+            data= playerGraphs,
+            layout=go.Layout(
+                title='Player Efficacies per Game',
+                showlegend=True,
+                legend=go.Legend(
+                    x=0,
+                    y=1.0
+                ),
+                margin=go.Margin(l=40, r=0, t=40, b=30)
+            )
+        ),
+        style={'height': 300},
+        id='my-graph'
+    )
 ])
 
 @app.callback(dash.dependencies.Output('display-value', 'children'),
               [dash.dependencies.Input('dropdown', 'value')])
+
 def display_value(value):
     if value == 'Justin Wei':
         return 'Justin Wei is dope!'
@@ -27,6 +60,8 @@ def display_value(value):
         return 'Hello, Jonathan! Welcome :-)'
     else:
         return 'Error: Who is Miles Todzo? Serra sucks at basketball'
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
